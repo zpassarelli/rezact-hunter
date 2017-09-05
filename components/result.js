bce44ebf3d14486e5c0246ab95a25562c4bdc125
Zach Passarelli <zachary.passarelli@gmail.com>
2017-09-05 15:44:00 -0600

    add animations and refine styling

--- components/result.js
+++ components/result.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, Animated } from 'react-native';
 import { Link } from 'react-router-native';
 
 import styles from '../styles';
@@ -7,18 +7,47 @@ import styles from '../styles';
 export default class Result extends React.Component {
   constructor(props){
     super(props);
-    let result = JSON.stringify(props.location.pathname).slice(9,-1);
-    this.state = {
-      victory: result
-    };
+    this.result = JSON.stringify(props.location.pathname).slice(9,-1);
+    this.resultText = 'Victory!';
+    if(this.result != 'win') this.resultText = 'Defeat.';
+    this.resultColor = 'lime';
+    if(this.result != 'win') this.resultColor = 'crimson';
+
+    this.springValue = new Animated.Value(0.5);
+  }
+
+  spring = () => {
+    this.springValue.setValue(0.5);
+    Animated.spring(this.springValue, {
+      toValue: 1,
+      friction: 5,
+      tension: 30,
+      useNativeDriver: true
+    }).start();
+  }
+
+  componentDidMount () {
+    this.spring();
   }
 
   render() {
     return(
       <View style={styles.container}>
-        <View style={{flex:4, paddingTop:30}}>
+        <View style={{flex:4, paddingTop:30, alignItems:'center'}}>
 
-          <Text style={styles.tutorialText}>{this.state.victory === 'win' ? 'Victory!' : 'Defeat.'}</Text>
+          <Animated.View style={[styles.logo,{transform:[{scale: this.springValue}]}]}>
+            <Text style={[styles.logoText,{color: this.resultColor}]}>
+              {this.resultText}
+            </Text>
+          </Animated.View>
+
+          <View style={[styles.button,{height:100}]}>
+            {this.result != 'win' ? (
+              <Text style={[styles.buttonText,{textAlign:'center'}]}>Your party was defeated.</Text>
+            ) : (
+              <Text style={[styles.buttonText,{textAlign:'center'}]}>The monster is defeated.</Text>
+            )}
+          </View>
 
         </View>
 
