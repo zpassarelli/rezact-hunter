e1e7f0d2f1cabd6ab8b4c9aab3192c6118c99a0d
Zach Passarelli <zachary.passarelli@gmail.com>
2017-09-06 17:49:56 -0600

    add damaged hp functionality

--- components/game_components/progress.js
+++ components/game_components/progress.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 export default class Progress extends React.Component {
+  constructor(props){
+    super(props);
+    this.timer = null;
+    this.state = {
+      dmg: 0
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({dmg: 140 * ( (this.props.hp - nextProps.hp) / this.props.maxhp)});
+  }
+
   render(){
     return (
       <View style={styles.border}>
         <View style={[styles.fill,{width: 140 *(this.props.hp/this.props.maxhp)}]}></View>
+        <View style={[styles.dmg,{width: this.state.dmg}]}></View>
       </View>
     )
   }
@@ -15,7 +28,7 @@ const styles = StyleSheet.create({
   border: {
     width: 144,
     height: 16,
-    zIndex: 5,
+    backgroundColor: 'rgba(0,0,0,0.5)',
     borderStyle: 'solid',
     borderColor: 'black',
     borderWidth: 2,
@@ -25,5 +38,9 @@ const styles = StyleSheet.create({
   fill: {
     height: 12,
     backgroundColor: 'lime'
+  },
+  dmg: {
+    height: 12,
+    backgroundColor: 'red'
   }
 });
